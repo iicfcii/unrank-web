d1199688031aeaf60f9af54ab2acbc6d35b93337
iicfcii <8230152+cfc3434@users.noreply.github.com>
2021-02-03 21:59:40 -0700

    Added hover info

--- src/Analysis/Objective.js
+++ src/Analysis/Objective.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Chart, Stack } from 'grommet';
+import { Box, Chart, Stack, Text } from 'grommet';
 import { StatBox } from './StatBox';
 
 const chartColor = (s) => {
@@ -9,7 +9,9 @@ const chartColor = (s) => {
 }
 
 export const Objective = (props) => {
-  const [hoverX, setHoverX] = useState(-1);
+  const [hoverPt, setHoverPt] = useState(null);
+  const [value, setValue] = useState(null);
+  const [range, setRange] = useState(null);
   const [dataGroups, setDataGroups] = useState([]);
 
   let data = props.data;
@@ -48,6 +50,7 @@ export const Objective = (props) => {
 
     console.log(dataGroupsNew)
     setDataGroups(dataGroupsNew);
+    setRange([0,time[time.length-1]]);
   },[data]);
 
   const dashedLine = {color:'#EFEFEF', size:'1px', side:'top', style:'dashed'};
@@ -59,9 +62,7 @@ export const Objective = (props) => {
       chartAreas.push(
         <Box key={i} fill='vertical' width={length/totalLength*100+'%'}>
           <Chart
-            size='fill'
-            type='area'
-            thickness='0px'
+            size='fill' type='area' thickness='0px'
             bounds={[[0,length],[0,100]]}
             values={g.values}/>
         </Box>
@@ -71,10 +72,8 @@ export const Objective = (props) => {
         <Box key={i} fill='vertical' width={length/totalLength*100+'%'}>
           <Chart
             key={i}
-            size='fill'
-            color={{color: g.color, opacity: '0.2'}}
-            type='area'
-            thickness='0px'
+            size='fill' color={{color: g.color, opacity: '0.2'}}
+            type='area' thickness='0px'
             bounds={[[0,length],[0,100]]}
             values={g.values}/>
         </Box>
@@ -89,9 +88,7 @@ export const Objective = (props) => {
       chartLines.push(
         <Box key={i} fill='vertical' width={length/totalLength*100+'%'}>
           <Chart
-            size='fill'
-            type='line'
-            thickness='0px'
+            size='fill' type='line' thickness='0px'
             bounds={[[0,length],[0,100]]}
             values={g.values}/>
         </Box>
@@ -101,10 +98,8 @@ export const Objective = (props) => {
         <Box key={i} fill='vertical' width={length/totalLength*100+'%'}>
           <Chart
             key={i}
-            size='fill'
-            color={g.color}
-            type='line'
-            thickness='3px'
+            size='fill' round color={g.color}
+            type='line' thickness='3px'
             bounds={[[0,length],[0,100]]}
             values={g.values}/>
         </Box>
@@ -130,11 +125,47 @@ export const Objective = (props) => {
         </Box>
         <Box fill border={{color:'#A9A9A9', size:'1px', side:'bottom', style:'solid'}}>
         </Box>
-        {hoverX>=0 && (
+        {hoverPt && (
           <Box
-            fill='vertical' overflow='hidden'
-            style={{marginLeft: hoverX+'px'}}
+            fill='vertical' style={{marginLeft: hoverPt[0]+'px'}}
             border={{color:'#737373', size:'1px', side:'left', style:'dashed'}}>
+          </Box>
+        )}
+        {hoverPt && (
+          <Box
+            style={{marginTop: hoverPt[1]+'px'}}
+            border={{color:'#737373', size:'1px', side:'top', style:'dashed'}}>
+          </Box>
+        )}
+        {hoverPt && (
+          <Box
+            style={{position: 'relative', left:`${hoverPt[0]-8}px`,top:`${hoverPt[1]-8}px`}}
+            width='16px' height='16px' round background='white'
+            border={{color:value[2]===1?'blue':'red', size:'4px', side:'all', style:'solid'}}>
+          </Box>
+        )}
+        {value && range && hoverPt && (
+          <Box
+            style={{
+              position: 'absolute',
+              left:`${hoverPt[0]+(value[0]>(range[1]-range[0])/2?-8:8)}px`,
+              top:`${hoverPt[1]+(value[1]<50?-8:8)}px`,
+              transform: `translate(
+                ${value[0]>(range[1]-range[0])/2?'-100%':'0%'},
+                ${value[1]<50?'-100%':'0%'}
+              )`,
+              width: 'fit-content'
+            }}
+            background='rgba(0,0,0,0.5)' pad='xsmall' round='xxsmall'>
+            <Text size='small' color='white'>{`队伍${value[2]}防守`}</Text>
+            <Box direction='row'>
+              <Text size='small' color='white'>{`进度：`}</Text>
+              <Text weight={900} size='small' color='white'>{`${value[1]}%`}</Text>
+            </Box>
+            <Box direction='row'>
+              <Text size='small' color='white'>{`时间：`}</Text>
+              <Text weight={900} size='small' color='white'>{`${value[0]}s`}</Text>
+            </Box>
           </Box>
         )}
         <Box
@@ -143,10 +174,22 @@ export const Objective = (props) => {
             let rect = event.target.getBoundingClientRect()
             let x = event.clientX - rect.left;
             // let y = event.clientY - rect.top;
-            setHoverX(x);
+
+            let t = Math.round(x/rect.width*(range[1]-range[0]));
+            let p = data.objective.progress[t];
+            let status = data.objective.status;
+
+            if (status[t] > 0) {
+              setValue([t,p,status[t]]);
+              setHoverPt([x,Math.round((100-p)/100*rect.height)]);
+            } else {
+              setValue(null);
+              setHoverPt(null);
+            }
           }}
           onMouseOut={() => {
-            setHoverX(-1);
+            setValue(null);
+            setHoverPt(null);
           }}>
         </Box>
       </Stack>
