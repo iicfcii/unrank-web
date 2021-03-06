2b624f4d2440bbc02f8749213488bc56e2a29f32
iicfcii <8230152+iicfcii@users.noreply.github.com>
2021-03-05 23:37:14 -0700

    Fixed default hidden

--- src/Analysis/DataViewer.js
+++ src/Analysis/DataViewer.js
@@ -7,8 +7,8 @@ import { Detail } from './Detail';
 
 export const DataViewer = ({data}) => {
   const [range, setRange] = useState([0,data.time.data.length]);
-  const [hideTable, setHideTable] = useState(false);
-  const [hideDetail, setHideDetail] = useState(true);
+  const [hideTable, setHideTable] = useState(true);
+  const [hideDetail, setHideDetail] = useState(false);
 
   useEffect(() => {
     setRange([0,data.time.data.length]);
